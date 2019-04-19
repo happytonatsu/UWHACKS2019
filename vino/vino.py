ff436ec66e6828cea932f44ffef6f732b7d47524
David Kennedy <huskydavidk@gmail.com>
2019-04-19 16:32:26 -0700

    routes added

--- vino/vino.py
+++ vino/vino.py
@@ -17,6 +17,14 @@ def main():
     def index():
         return render_template('index.html')
 
+    @app.route('/static/<path:path>')
+    def send_static(path):
+    return send_from_directory('static', path)
+
+    
+    @app.route('/static/images')
+    def send_js(path):
+
 
     @app.route('/winery')
     @app.route('/winery/<int:_id>')
